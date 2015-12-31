92440974ff7cdad1feac8cbcbd08d55721a6dede
Yuriy Taraday <yorik.sar@gmail.com>
2015-12-31 18:25:04 +0300

    Add mixin for models with fancy __repr__ and id

--- tuning_box/db.py
+++ tuning_box/db.py
@@ -15,6 +15,7 @@ import json
 
 import flask_sqlalchemy
 import sqlalchemy.event
+import sqlalchemy.ext.declarative as sa_decl
 from sqlalchemy import types
 
 db = flask_sqlalchemy.SQLAlchemy()
@@ -24,6 +25,19 @@ pk = functools.partial(db.Column, pk_type, primary_key=True)
 
 def fk(cls, **kwargs):
     return db.Column(pk_type, db.ForeignKey(cls.id), **kwargs)
+
+
+class ModelMixin(object):
+    id = db.Column(pk_type, primary_key=True)
+
+    def __repr__(self):
+        args = []
+        for attr in self.__repr_attrs__:
+            value = getattr(self, attr)
+            if attr == 'content' and value is not None and len(value) > 15:
+                value = value[:10] + '<...>'
+            args.append('{}={!r}'.format(attr, value))
+        return '{}({})'.format(type(self).__name__, ','.join(args))
 
 
 class Json(types.TypeDecorator):
@@ -36,33 +50,37 @@ class Json(types.TypeDecorator):
         return json.loads(value)
 
 
-class Namespace(db.Model):
-    id = pk()
+class Namespace(ModelMixin, db.Model):
     name = db.Column(db.String(128))
+    __repr_attrs__ = ('id', 'name')
 
 # Component registry
 
 
-class Component(db.Model):
-    id = pk()
+class Component(ModelMixin, db.Model):
     name = db.Column(db.String(128))
     schemas = db.relationship("Schema", backref="component")
     templates = db.relationship("Template", backref="component")
 
+    __repr_attrs__ = ('id', 'name')
 
-class Schema(db.Model):
-    id = pk()
+
+class Schema(ModelMixin, db.Model):
     name = db.Column(db.String(128))
     component_id = fk(Component)
     namespace_id = fk(Namespace)
+    namespace = db.relationship(Namespace)
     content = db.Column(Json)
 
+    __repr_attrs__ = ('id', 'name', 'component', 'namespace', 'content')
 
-class Template(db.Model):
-    id = pk()
+
+class Template(ModelMixin, db.Model):
     name = db.Column(db.String(128))
     component_id = fk(Component)
     content = db.Column(Json)
+
+    __repr_attrs__ = ('id', 'name', 'component', 'content')
 
 # Environment data storage
 
@@ -73,26 +91,30 @@ _environment_components = db.Table(
 )
 
 
-class Environment(db.Model):
-    id = pk()
+class Environment(ModelMixin, db.Model):
     components = db.relationship(Component, secondary=_environment_components)
 
+    __repr_attrs__ = ('id',)
 
-class EnvironmentHierarchyLevel(db.Model):
-    id = pk()
+
+class EnvironmentHierarchyLevel(ModelMixin, db.Model):
     environment_id = fk(Environment)
     environment = db.relationship(Environment, backref='hierarchy_levels')
     name = db.Column(db.String(128))
     parent_id = db.Column(pk_type,
                           db.ForeignKey('environment_hierarchy_level.id'))
-    parent = db.relationship('EnvironmentHierarchyLevel',
-                             backref=db.backref('child', uselist=False),
-                             remote_side=[id])
+
+    @sa_decl.declared_attr
+    def parent(cls):
+        return db.relationship(cls,
+                               backref=db.backref('child', uselist=False),
+                               remote_side=cls.id)
 
     __table_args__ = (
         db.UniqueConstraint(environment_id, name),
         db.UniqueConstraint(environment_id, parent_id),
     )
+    __repr_attrs__ = ('id', 'environment', 'parent', 'name')
 
     @classmethod
     def get_for_environment(cls, environment):
@@ -106,19 +128,21 @@ class EnvironmentHierarchyLevel(db.Model):
         return env_levels
 
 
-class EnvironmentHierarchyLevelValue(db.Model):
-    id = pk()
+class EnvironmentHierarchyLevelValue(ModelMixin, db.Model):
     level_id = fk(EnvironmentHierarchyLevel)
     level = db.relationship(EnvironmentHierarchyLevel)
     parent_id = db.Column(
         pk_type, db.ForeignKey('environment_hierarchy_level_value.id'))
-    parent = db.relationship('EnvironmentHierarchyLevelValue',
-                             remote_side=[id])
     value = db.Column(db.String(128))
 
+    @sa_decl.declared_attr
+    def parent(cls):
+        return db.relationship(cls, remote_side=cls.id)
 
-class EnvironmentSchemaValues(db.Model):
-    id = pk()
+    __repr_attrs__ = ('id', 'level', 'parent', 'value')
+
+
+class EnvironmentSchemaValues(ModelMixin, db.Model):
     environment_id = fk(Environment)
     environment = db.relationship(Environment)
     schema_id = fk(Schema)
@@ -130,6 +154,7 @@ class EnvironmentSchemaValues(db.Model):
     __table_args__ = (
         db.UniqueConstraint(environment_id, schema_id, level_value_id),
     )
+    __repr_attrs__ = ('id', 'environment', 'schema', 'level_value', 'values')
 
 
 def get_or_create(cls, **attrs):
